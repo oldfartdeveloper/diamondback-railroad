fc4602e967285a0831472f23f56ae300a3f618bd
Scott Smith <scottnelsonsmith@gmail.com>
2016-07-04 15:06:04 -0700

    compiles; shows simple text message as placeholder

--- Console.elm
+++ Console.elm
@@ -23,6 +23,13 @@ import Task
 import Window
 import ControlPanel
 import Game
+import Keyboard exposing (KeyCode)
+import AnimationFrame
+import Time exposing (Time, second, millisecond)
+import Matrix exposing (Location)
+import Position
+import Piece
+
 
 
 type alias Model =
@@ -38,6 +45,8 @@ init =
             ControlPanel.init
       , game =
             Game.init
+      , windowSize =
+            Window.Size 0 0
       }
     , Task.perform (\_ -> Idle) (\x -> Resize x) Window.size
     )
@@ -46,11 +55,43 @@ init =
 type Msg
     = Resize Window.Size
     | Idle
+    | KeyDown KeyCode
+    | Animate Time
+    | Blink Time
+    | ModifyPosition Location Position.Msg
+    | ModifyPiece Location Piece.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        ModifyPosition location positionMsg ->
+            ( model, Cmd.none )
+
+        ModifyPiece location pieceMsg ->
+            ( model, Cmd.none )
+
+        KeyDown keyCode ->
+            ( model, Cmd.none )
+
+        --            manageKeyDown model keyCode
+        Blink time ->
+            ( model, Cmd.none )
+
+        --            blinkUnvisitedPerimeterPositions model
+        Animate time ->
+            ( model, Cmd.none )
+
+        {-
+           ( { model
+               | chain =
+                   (List.map (\piece -> animatePiece piece time)
+                       model.chain
+                   )
+             }
+           , Cmd.none
+           )
+        -}
         Resize newSize ->
             ( { model
                 | windowSize = newSize
@@ -60,6 +101,16 @@ update msg model =
 
         Idle ->
             ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Keyboard.downs KeyDown
+        , AnimationFrame.times Animate
+        , Time.every (700 * Time.millisecond) Blink
+        , Window.resizes Resize
+        ]
 
 
 view : Model -> Html Msg
